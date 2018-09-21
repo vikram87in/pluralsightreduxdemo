659951ac70a91c2b17b9e144e83a030245931e68
vikram singh <vikram87in@gmail.com>
2018-09-21 10:49:21 +0530

    used stage-2 preset in babel to enable arrow functions

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -4,33 +4,30 @@ class CoursesPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            course:{
-                title:null
+            course: {
+                title: ''
             }
         };
-        this.onTitleChange = this.onTitleChange.bind(this);
-        this.onClickSave = this.onClickSave.bind(this);
     }
 
-    onTitleChange(e){
-// // let val = e.target.value;
-// let course = this.state.course;
-// course.title = e.target.value;
-// this.setState({course});
+    onTitleChange = (e) => {
+        let course = this.state.course;
+        course.title = e.target.value;
+        this.setState({ course });
     }
 
-    onClickSave(){
-
+    onClickSave = () => {
+        alert(`saving... ${this.state.course.title}`);
     }
-    
+
     render() {
         return (
             <div>
                 <h1>Courses</h1>
-                <h2>Add course</h2> 
-                <input type="text" onChange={this.onTitleChange} value={this.state.course.title}/>
-                <input type="submit" value="Save" onClick={this.onClickSave}/>
-                
+                <h2>Add course</h2>
+                <input type="text" onChange={this.onTitleChange} value={this.state.course.title} />
+                <input type="submit" value="Save" onClick={this.onClickSave} />
+
             </div>
         );
     }
